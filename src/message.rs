d9c6555361e5d0f6bdfc6c3654d96d55dee3298f
PabloSanchi <pablo.sanchi.herrera@gmail.com>
2025-09-05 23:49:06 +0200

    refactor: update msg handling

--- src/message.rs
+++ src/message.rs
@@ -1,13 +1,119 @@
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
+#[derive(Clone, Serialize, Deserialize)]
 pub struct Message {
     pub src: String,
     pub dest: String,
     pub body: Body,
 }
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
+impl Message {
+    pub fn new(message: &Message, payload: Payload) -> Self {
+        Self {
+            src: message.dest.clone(),
+            dest: message.src.clone(),
+            body: Body {
+                msg_id: None,
+                in_reply_to: message.body.msg_id,
+                payload: payload.to_owned(),
+            },
+        }
+    }
+}
+
+pub struct MessageBuilder {
+    src: Option<String>,
+    dest: Option<String>,
+    msg_id: Option<usize>,
+    in_reply_to: Option<usize>,
+    payload: Option<Payload>,
+}
+
+impl Default for MessageBuilder {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl MessageBuilder {
+    pub fn new() -> Self {
+        Self {
+            src: None,
+            dest: None,
+            msg_id: None,
+            in_reply_to: None,
+            payload: None,
+        }
+    }
+
+    pub fn from(msg: &Message) -> Self {
+        let Message {
+            src: to,
+            dest: from,
+            body,
+        } = msg.to_owned();
+        let Body { msg_id, .. } = body;
+
+        Self {
+            src: Some(from),
+            dest: Some(to),
+            msg_id: None,
+            in_reply_to: msg_id,
+            payload: None,
+        }
+    }
+
+    pub fn with_src(mut self, src: &str) -> Self {
+        self.src = Some(src.to_owned());
+        self
+    }
+
+    pub fn with_dest(mut self, dest: &str) -> Self {
+        self.dest = Some(dest.to_owned());
+        self
+    }
+
+    pub fn with_msg_id(mut self, msg_id: usize) -> Self {
+        self.msg_id = Some(msg_id);
+        self
+    }
+
+    pub fn with_in_reply_to(mut self, in_reply_to: usize) -> Self {
+        self.in_reply_to = Some(in_reply_to);
+        self
+    }
+
+    pub fn with_payload(mut self, payload: Payload) -> Self {
+        self.payload = Some(payload);
+        self
+    }
+
+    pub fn build(self) -> Message {
+        if self.src.is_none() {
+            panic!("Message src must be defined!")
+        }
+
+        if self.dest.is_none() {
+            panic!("Message dest must be defined!")
+        }
+
+        if self.payload.is_none() {
+            panic!("Message payload must be defined!")
+        }
+
+        Message {
+            src: self.src.unwrap(),
+            dest: self.dest.unwrap(),
+            body: Body {
+                msg_id: self.msg_id,
+                in_reply_to: self.in_reply_to,
+                payload: self.payload.unwrap(),
+            },
+        }
+    }
+}
+
+#[derive(Clone, Serialize, Deserialize)]
 pub struct Body {
     #[serde(skip_serializing_if = "Option::is_none")]
     pub msg_id: Option<usize>,
@@ -18,17 +124,28 @@ pub struct Body {
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct EmptyPayload {}
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct InitPayload {
+    pub node_id: String,
+    pub node_ids: Vec<String>,
+}
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct EchoPayload {
+    pub echo: String,
+}
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct GeneratePayload {
+    pub id: String,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
 #[serde(tag = "type", rename_all = "snake_case")]
 pub enum Payload {
-    Echo {
-        echo: String,
-    },
-    EchoOk {
-        echo: String,
-    },
-    Init {
-        node_id: String,
-        node_ids: Vec<String>,
-    },
-    InitOk {},
+    Init(InitPayload),
+    InitOk(EmptyPayload),
+    Echo(EchoPayload),
+    EchoOk(EchoPayload),
+    Generate(EmptyPayload),
+    GenerateOk(GeneratePayload),
 }
--- src/node.rs
+++ src/node.rs
@@ -1,12 +1,18 @@
-use crate::message::{Body, Message, Payload};
+use crate::message::{
+    EchoPayload, EmptyPayload, GeneratePayload, InitPayload, Message, MessageBuilder, Payload,
+};
 use crate::writer::NewLineWriter;
-use log::info;
-use rand::Rng;
 use std::io::StdoutLock;
 use std::sync::OnceLock;
 
 pub struct Node {
     node_id: OnceLock<String>,
+}
+
+impl Default for Node {
+    fn default() -> Self {
+        Self::new()
+    }
 }
 
 impl Node {
@@ -21,72 +27,62 @@ impl Node {
         input_msg: Message,
         output: &mut NewLineWriter<StdoutLock>,
     ) -> std::io::Result<()> {
-        if let Some(reply_msg) = self.handle_msg(input_msg) {
+        if let Some(reply_msg) = self.generate_msg(&input_msg) {
             output.write(&reply_msg)?;
         }
 
         Ok(())
     }
 
-    // visible for testing
-    pub fn handle_msg(&mut self, input: Message) -> Option<Message> {
-        let Message {
-            src: dest, body, ..
-        } = input;
-        let Body {
-            msg_id, payload, ..
-        } = body;
-
-        match payload {
-            Payload::Echo { echo } => {
-                let src = self.src().to_owned();
-                let msg = Message {
-                    src: src.clone(),
-                    dest: dest,
-                    body: Body {
-                        msg_id: Some(generate_id()),
-                        in_reply_to: msg_id,
-                        payload: Payload::EchoOk { echo },
-                    },
-                };
-
-                Some(msg)
-            }
-            Payload::Init { node_id, .. } => {
-                let _ = self.node_id.set(node_id);
-
-                let src = self.src().to_owned();
-                let msg = Message {
-                    src: src.clone(),
-                    dest: dest,
-                    body: Body {
-                        msg_id: Some(generate_id()),
-                        in_reply_to: msg_id,
-                        payload: Payload::InitOk {},
-                    },
-                };
-
-                info!("EchoNode {} was initialized", src.clone());
-
-                Some(msg)
-            }
-            Payload::InitOk {} => panic!("EchoNode should never receive an InitOk message"),
-            Payload::EchoOk { .. } => None,
+    #[inline]
+    fn assert_initialized(&self) {
+        if self.node_id.get().is_none() {
+            panic!("Node was not initalized!")
         }
     }
-
-    fn src(&self) -> &str {
-        self.node_id.get().expect("EchoNode was not initialized!")
-    }
 }
 
-impl Default for Node {
-    fn default() -> Self {
-        Self::new()
-    }
+pub trait MessageGenerator {
+    fn generate_msg(&mut self, message: &Message) -> Option<Message>;
+
+    fn handle_init(&mut self, payload: InitPayload) -> Payload;
+    fn handle_echo(&self, payload: EchoPayload) -> Payload;
+    fn handle_generate(&self) -> Payload;
 }
 
-fn generate_id() -> usize {
-    let mut rng = rand::rng();
-    rng.random::<u64>() as usize
+impl MessageGenerator for Node {
+    fn generate_msg(&mut self, message: &Message) -> Option<Message> {
+        use Payload::*;
+        let generated_payload = match message.body.payload.clone() {
+            Init(init_payload) => self.handle_init(init_payload),
+            Echo(echo_payload) => self.handle_echo(echo_payload),
+            Generate(_) => self.handle_generate(),
+            _ => panic!("Node received invalid message!"),
+        };
+
+        let msg = MessageBuilder::from(&message)
+            .with_msg_id(1) // todo: use a distributed id generator
+            .with_payload(generated_payload)
+            .build();
+
+        Some(msg)
+    }
+
+    fn handle_init(&mut self, payload: InitPayload) -> Payload {
+        let InitPayload { node_id, .. } = payload;
+        let _ = self.node_id.set(node_id);
+        Payload::InitOk(EmptyPayload {})
+    }
+
+    fn handle_echo(&self, payload: EchoPayload) -> Payload {
+        self.assert_initialized();
+        Payload::EchoOk(payload)
+    }
+
+    fn handle_generate(&self) -> Payload {
+        self.assert_initialized();
+        Payload::GenerateOk(GeneratePayload {
+            id: String::from("some id"),
+        })
+    }
 }
